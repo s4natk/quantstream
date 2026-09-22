3ffa0c985fd07e407598c2bdf78360727fd607f9
Sanat Kanwal <kanwalsanat@gmail.com>
2026-09-21 21:53:08 -0400

    Test the alert query

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,6 +1,6 @@
 from sqlalchemy.dialects import postgresql
 
-from quantstream.queries import recent_candles_stmt, upsert_candle_stmt
+from quantstream.queries import recent_alerts_stmt, recent_candles_stmt, upsert_candle_stmt
 from tests.support import make_candle
 
 
@@ -19,3 +19,11 @@ def test_recent_candles_filter_and_sort():
     assert "order by" in lowered
     assert "desc" in lowered
     assert "10" in lowered
+
+
+def test_recent_alerts_filter_by_symbol():
+    statement = recent_alerts_stmt("IBM", 5)
+    lowered = str(statement.compile(compile_kwargs={"literal_binds": True})).lower()
+    assert "ibm" in lowered
+    assert "alerts" in lowered
+    assert "5" in lowered
